0ae5e27a457ff622f5560da2f240a43f7c49e932
0Sam120 <114755608+0Sam120@users.noreply.github.com>
2023-11-24 13:50:49 +0800

    Add files via upload

---
+++ autoShop/autoShop/МенюГостя.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace autoShop
+{
+    public partial class МенюГостя : Form
+    {
+        public МенюГостя()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 logForm = new Form2();
+            logForm.Show();
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Заказы orders = new Заказы();
+            orders.Show();
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ДобавлениеКлиента client = new ДобавлениеКлиента();
+            client.Show();
+            this.Close();
+        }
+    }
+}
